63d3cc6124fcc02163e32cd395a212e32c77af66
jatinder <jatinder1.singh@trantorinc.com>
2022-07-19 18:59:45 +0530

    refactor: add basic styles

--- ui/src/pages/public/calculator/index.tsx
+++ ui/src/pages/public/calculator/index.tsx
@@ -3,21 +3,21 @@ import React, { useState } from "react";
 import "./style.scss";
 
 const numberButtons = [
-  { buttonId: "seven", buttonValue: "7" },
-  { buttonId: "eight", buttonValue: "8" },
-  { buttonId: "nine", buttonValue: "9" },
-  { buttonId: "four", buttonValue: "4" },
-  { buttonId: "five", buttonValue: "5" },
-  { buttonId: "six", buttonValue: "6" },
-  { buttonId: "one", buttonValue: "1" },
-  { buttonId: "two", buttonValue: "2" },
-  { buttonId: "three", buttonValue: "3" },
-  { buttonId: "zero", buttonValue: "0" },
+  { id: "seven", value: "7" },
+  { id: "eight", value: "8" },
+  { id: "nine", value: "9" },
+  { id: "four", value: "4" },
+  { id: "five", value: "5" },
+  { id: "six", value: "6" },
+  { id: "one", value: "1" },
+  { id: "two", value: "2" },
+  { id: "three", value: "3" },
+  { id: "zero", value: "0" },
 ];
 
-const CalButton = (p:{ id:string, handleClick: any, value:string }) => {
+const CalButton = (p: { id: string; handleClick: any; value: string; className?: string }) => {
   return (
-    <div className="cal-item" id={p.id} onClick={p.handleClick}>
+    <div className={`cal-item ${p.className || ""}`} id={p.id} onClick={p.handleClick}>
       {p.value}
     </div>
   );
@@ -37,7 +37,7 @@ const Calculator = () => {
     setLastInput("");
   };
 
-  const handleNumClick = (e:any) => {
+  const handleNumClick = (e: any) => {
     let input = e.target.id;
     console.log("-----handlenumclikc===", input);
 
@@ -89,7 +89,6 @@ const Calculator = () => {
           setDisplayInput(displayInput.concat(numbers.indexOf(input).toString()));
           setLastInput(numbers.indexOf(input).toString());
         } else {
-          console.log("---type--num-");
           setDisplayInput(numbers.indexOf(input).toString());
           setLastInput(numbers.indexOf(input).toString());
         }
@@ -101,7 +100,7 @@ const Calculator = () => {
     }
   };
 
-  const computeFormula = (formula:string, input:string) => {
+  const computeFormula = (formula: string, input: string) => {
     let finalFormula = formula.concat(input);
     let answer = eval(finalFormula);
     answer = +answer.toFixed(6);
@@ -120,25 +119,19 @@ const Calculator = () => {
         </div>
 
         <div className="cal-wrapper">
-          <div id="number-pad">
-            {numberButtons.map((numObj, i, numArr) => {
-              return (
-                <div key={i}>
-                  <CalButton
-                    id={numArr[i].buttonId}
-                    value={numArr[i].buttonValue}
-                    handleClick={handleNumClick}
-                  />
-                </div>
-              );
-            })}
+          <div className="number-pad">
+            {numberButtons.map((numObj: { id: string; value: string }, i: number) => (
+              <span key={i}>
+                <CalButton id={numObj.id} value={numObj.value} handleClick={handleNumClick} />
+              </span>
+            ))}
           </div>
-          <div id="other-buttons">
+          <div className="other-buttons">
             <CalButton id="add" value="+" handleClick={handleNumClick} />
             <CalButton id="subtract" value="-" handleClick={handleNumClick} />
             <CalButton id="multiply" value="&times;" handleClick={handleNumClick} />
             <CalButton id="divide" value="&divide;" handleClick={handleNumClick} />
-            <CalButton id="clear" value="CLEAR" handleClick={handleNumClick} />
+            <CalButton id="clear" value="CE" handleClick={handleNumClick} />
             <CalButton id="equals" value="=" handleClick={handleNumClick} />
           </div>
         </div>
